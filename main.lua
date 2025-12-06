ca10eb5b8015f0bb5565e187fe70be37c64430e4
Gabriele Corti <dernier.grie@gmail.com>
2025-12-06 09:10:32 +0100

    input workaround
    inspired by gd50 to consider user input through the love.keyboard monkey-patched module

--- main.lua
+++ main.lua
@@ -6,10 +6,14 @@ local world = World:new()
 function love.load()
     love.window.setTitle(SCREEN_TITLE)
     love.window.setMode(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    love.keyboard.keypressed = {}
 end
 
 function love.update(dt)
+    -- update logic
 
+    love.keyboard.keypressed = {}
 end
 
 function love.draw()
@@ -19,9 +23,15 @@ function love.draw()
     screen:finish()
 end
 
+function love.keyboard.waspressed(key)
+    return love.keyboard.keypressed[key] == true
+end
+
 -- quit on escape
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
     end
+
+    love.keyboard.keypressed[key] = true
 end
